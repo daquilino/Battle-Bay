e3ebd62bdf9c455db51ff967f8dab3aa9f1cf768
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-05-05 23:21:24 -0400

    started coding warehouse-items-api-routes.js, added added timestamps: false to warehouse-items.js model

--- controllers/warehouse-items-api-routes.js
+++ controllers/warehouse-items-api-routes.js
@@ -11,7 +11,80 @@ const DB = require("../models");
 // =============================================================
 module.exports = function(app) 
 {
+	// get all data from warehouseItems table
+	app.get("/api/warehouse", function(req, res)
+	{
+		DB.warehouseItems.findAll()
+		.then(function(data)
+		{
+			res.json(warehousePrices);
+		});		
+	});
+
+
 	
+	
+	/*If in jQuery send body object 
+			{
+				id: ,
+				units_sold: 
+			}		
+	data in callback should be data of item with id just updated? check. */
+
+	// use to update `units sold`  warehouseItems
+	app.put("/api/warehouse", function(req, res)
+	{
+		
+		DB.warehouseItems.update(
+		{ 
+			units_sold: req.body.units_sold
+		}
+		,
+		{
+			where: 
+			{
+				id: req.body.id
+			}
+		})
+		.then(function(data)
+		{
+			res.json(data);  
+		});
+
+	});
+
+// ====================== Optional Routes =======================================
+/*				
+				// Add items to warehouse
+				app.post("/api/warehouse", function(req, res)
+				{
+			console.log("\n\nCREATE");
+			console.log(JSON.stringify(req.body, null, 2));
+					
+					DB.warehouseItems.create()
+					.then(function(data)
+					{
+						res.json(data);  
+					});		
+				});
+
+
+				//Delete item from warehouse
+				app.delete("/api/posts/:id", function(req, res) 
+				{
+				    DB.warehouseItems.destroy(
+				    {
+				      where: 
+				      {
+				        id: req.params.id  
+				      }
+				    }).then(function(dbPost) 
+				    {
+				      res.json(dbPost);
+				    });
+			  	});
+			
+*/	
 
   
 };
---
+++ db/warehouse-seeds.sql
@@ -0,0 +1,4 @@
+Use battleBay_db;
+
+INSERT INTO warehouseItems (item_name, category, units_sold, price, warehouse_name) 
+VALUES ("item1", "electronics", 5, 20, "crazy eddys");
