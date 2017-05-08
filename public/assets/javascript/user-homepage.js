2bcce261617779a98194b38f0835925f56b08519
nova1023 <thomas.yu1023@gmail.com>
2017-05-08 16:10:18 -0400

    created leaderboard html, merged all CSS into style.css, added jquery css and warehouse-page.js to allhtml files, routed leaderboard in html routes, wrapped sign-in-button with <a> to route to user-homepage

---
+++ public/assets/javascript/user-homepage.js
@@ -0,0 +1,69 @@
+// GET Player account
+//-------------------------------------------------------------------------------------
+
+function getPlayerAccountInfo() {
+	console.log("Get Player Account Info Button Pressed.");
+
+	var id = 0;
+
+	$.get("api/user/" + id, function(data){
+
+		var username = data.username;
+		var balance = data.balance;
+		var money_spent = data.money_spent;
+		var money_earned = data.money_earned;
+
+
+		$("#playerAccountInfo").empty();
+		$("#playerAccountInfo").append("<h4>" + username + "</h4>");
+		$("#playerAccountInfo").append("<p>Balance: " + balance + "</p>");
+		$("#playerAccountInfo").append("<p>Money Spent: " + money_spent + "</p>");
+		$("#playerAccountInfo").append("<p>Money Earned: " + money_earned + "</p>");
+
+	});
+}
+
+// GET player listings
+//-------------------------------------------------------------------------------------
+
+function getPlayerListings() {
+	console.log("Get Player Listings Button Pressed.");
+
+	var id = 0;
+
+	$("#playerListings").empty();
+	$("#playerListings").append("<h4>Your Active Listings</h4>");
+
+	$.get("api/users/" + id + "/listings", function(data){
+
+		var numberOfListings = 5;
+
+		for (var i = 0; i < numberOfListings; i++){
+			$("#playerListings").append("<p>=-=-=-=-=-=Listing " + i + " =-=-=-=-=-=</p>");
+		}
+	});
+
+}
+
+//=====================================================================================
+
+// GET players items sold
+//-------------------------------------------------------------------------------------
+
+function getPlayersItemsSold() {
+	console.log("Get Players Items Sold.");
+
+	var id = 0;
+
+	$("#playersItemsSold").empty();
+	$("#playersItemsSold").append("<h4>Your Inactive Listings</h4>");
+
+	$.get("api/users/" + id + "/listings", function(data){
+
+		var numberOfListings = 5;
+
+		for (var i = 0; i < numberOfListings; i++){
+			$("#playersItemsSold").append("<p>=-=-=-=-=-=Listing " + i + " =-=-=-=-=-=</p>");
+		}
+	});
+}
