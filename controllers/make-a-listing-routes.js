a951f4b2e34d8faae86c85e3a84bba104ea004f4
Daniel Cross <dancross09@gmail.com>
2017-05-09 16:02:31 -0400

    Continue implementing 'make-a-listing'.

--- controllers/make-a-listing-routes.js
+++ controllers/make-a-listing-routes.js
@@ -2,21 +2,35 @@
 const DB = require("../models");
 
 //functions
-function EnoughInInvetory(reqBody)
+function EnoughInInvetory(reqBody, findingDifference)
 {
+	//this funtion serves two separate purposes
+		//determine if user has enough inventory to make sale
+		//find difference between inventory quantity and quantity listed
+
 	//check item type being sold
 	switch(reqBody.itemName)
 	{
 		case "fashion":
-			return (parseInt(reqBody.numFashion) >= parseInt(reqBody.quantity));
+			if (findingDifference)
+				return (parseInt(reqBody.numFashion) - parseInt(reqBody.quantity)); //int
+			else
+				return (parseInt(reqBody.numFashion) >= parseInt(reqBody.quantity)); //boolean
 			break;
 		case "electronics":
-			return (parseInt(reqBody.numElectronics) >= parseInt(reqBody.quantity));
+			if (findingDifference)
+				return (parseInt(reqBody.numElectronics) - parseInt(reqBody.quantity));
+			else
+				return (parseInt(reqBody.numElectronics) >= parseInt(reqBody.quantity));
 			break;
 		case "collectables":
-			return (parseInt(reqBody.numCollectables) >= parseInt(reqBody.quantity));
+			if (findingDifference)
+				return (parseInt(reqBody.numCollectables) - parseInt(reqBody.quantity));
+			else
+				return (parseInt(reqBody.numCollectables) >= parseInt(reqBody.quantity));
 			break;
 		default:
+			//shouldn't be able to enter default case
 			return false;
 			break;
 	}
@@ -33,20 +47,61 @@ module.exports = function(app)
 		console.log(req.body);
 
 		//if user has enough of inventory to make that sale
-		if (EnoughInInvetory(req.body))
+		if (EnoughInInvetory(req.body, false))
 		{
 			if (req.body.price > 0)
 			{
+				//make listing in itemsForSale table
+				DB.itemsForSale.create(
+				{
+					item_name: req.body.itemName,
+					quantity: parseInt(req.body.quantity),
+					starting_price: parseInt(req.body.price),
+					createdAt: new Date(),
+					allUserId: parseInt(req.cookies.id)
 
+				}).then(function(newListing)
+				{
+					console.log("---------- new listing created -------------");
+					res.redirect("/user-homepage");
+				});
+
+				//update quantity remaining in usersInventory
+				DB.usersInventory.update(
+				{
+					quantity: EnoughInInvetory(req.body, true)
+				}, 
+				{
+					where: 
+					{
+						allUserId: req.cookies.id,
+						item_name: req.body.itemName
+					}
+				}).then(function(updatedItem)
+				{
+					console.log("---------- user inventory item updated ----------");
+					console.log(updatedItem.dataValues);
+					res.redirect("/user-homepage");
+				});
 			}
 			else //price < 1
 				res.json({error: "Starting price must be greater than 0"});
-
-
 		}
 		else //not enough in inventory
 			res.json({error: "Not enough in stock"});
-				//make listing in itemsForSale table
 				//subtract the quantity being sold from user's inventory
 	});
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
