8ea1c3a3f08243bbd09bf9595029c42b708b2400
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-05-09 16:18:23 -0400

    removed code to make purchase, added findOne query

--- controllers/warehouse-items-api-routes.js
+++ controllers/warehouse-items-api-routes.js
@@ -22,112 +22,15 @@ module.exports = function(app)
 		});		
 	});
 
-			
-	// use to update `units sold` 
-	app.put("/api/warehouse", function(req, res)
-	{		
-			
-
-		addToUserInventory(req.body.userID, req.body.quantity, req.body.warehouseName, res);
-		updateUserStats(req.body.userID, req.body.total,res );
-
-		DB.warehouseItems.update(
-		{ 			
-			 units_sold: DB.sequelize.literal('units_sold + ' + req.body.quantity)				
-		},
-		
-		{
-			where: 
-			{
-				id: req.body.warehouseID
-			}
-		})
+	// get one item from 'warehouseItems'
+	app.get("/api/warehouse", function(req, res)
+	{
+		DB.warehouseItems.findOne({where: {id: req.body.id}})
 		.then(function(data)
-		{			
-			//res.json(data);  
-		});
-
+		{
+			res.json(data);
+		});		
 	});
-
-// ====================== Optional Routes To Add/Delete Items ================================
-/*				
-				// Add items to warehouse
-				app.post("/api/warehouse", function(req, res)
-				{				
-					DB.warehouseItems.create(req.body)
-					.then(function(data)
-					{
-						res.json(data);  
-					});		
-				});
-
-
-/*
-				//Delete item from warehouse
-				app.delete("/api/posts/:id", function(req, res) 
-				{
-				    DB.warehouseItems.destroy(
-				    {
-				      where: 
-				      {
-				        id: req.params.id  
-				      }
-				    }).then(function(dbPost) 
-				    {
-				      res.json(dbPost);
-				    });
-			  	});
-			
-*/	
-
-  
 };
 
-// helper functions
-
-//
-function addToUserInventory(userId, quantity, itemName, res)
-{
-
-console.log("\n\nADDTOUSERINVENTORY\n\n");	
-	
-	
-
-	var item ={	 
-		
-		quantity: parseInt(quantity),
-		item_name: itemName,
-		allUserId: 2          //userId
-	};
-
-console.log("\n\nitem", item, "\n\n")
-		
-	DB.usersInventory.create(item)
-	.then(function(data)
-	{
-		
-	});		
-	
-}
-
-
-//
-function updateUserStats(userId, totalSpent, res )
-{
-
-	userId = 2; //TEST CODE REMOVE
-
-	var updateObj = {
-		balance: DB.sequelize.literal('balance - ' + totalSpent),
-		money_spent: DB.sequelize.literal('money_spent + ' + totalSpent)
-	};
-
-	DB.allUsers.update(updateObj, {where: {id: userId}})
-	.then(function(data)
-	{
-		
-	});	
-
-
-}
 
