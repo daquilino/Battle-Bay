0a655d895aa4b320f539d2c3904ee81f6ae5d84c
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-05-10 16:44:49 -0400

    Code finished need to test

--- monitor-auctions.js
+++ monitor-auctions.js
@@ -8,79 +8,68 @@ const DB = require("./models");
 
 */
 
-/*
-If there are items for sale. check to see if completed (current_timestamp - createAt < auctionTime)
-	- if complete did the sell (highest_bid > starting_price || highest_bid !== null)			
-			
-			YES - remove from items-for-sale, update all-users stats, move back to userInventory setting sold to true.
-										
-			No - remove from items-for-sale, move back to userInventory.	
-
-*/
-
-
-
 //Assume 5 minute auctions for testing.
 
-
-module.exports = function(app)
+module.exports = function monitorAI(app)
 {
 	console.log("\nmonitor-autions function called\n"); //TEST CODE REMOVE
-
+	
 	//all array of items for sale obects.
 	var itemsForSale;
 	
-	const auctionTime = 5000; //miliseconds
+	//flag to check if interval is active
+	var intervalFlag = false;
+
+	//const auctionTime = 5000; //miliseconds NOT CURRENTLY USED MAY REMOVE hard codded now
 
 	
-	
-	// This should be in a interval timer a little less than auctionTime
-	// maybe put in its own function.
 	DB.itemsForSale.findAll()
 	.then(function(data)
 	{
 		itemsForSale = JSON.parse(JSON.stringify(data));  //this removes extra stuff from objects?
+
 			
-		//// ==============  test time code ==================
-		 //var currentItem = itemsForSale[5];	//TEST CODE REMOVE
-	
-		var intervalID = setInterval(function()
+		if(itemsForSale.length > 0)
 		{
-			if(itemsForSale.length > 0)
+			intervalFlag = true;
+			
+			//Runs every 5s until clearInterval is called.
+			var intervalID = setInterval(function()
 			{
-				for(var key in itemsForSale)
-				{
-					var currentItem = itemsForSale[key];
-
-					if(isExpired(currentItem)) // can send in auctionTime 
+				
+					for(var key in itemsForSale)
 					{
-						//itemsForSale.splice(key,1); // this will remove item from for same array.
+						var currentItem = itemsForSale[key];
+
+						if(isExpired(currentItem)) // may use 'auctionTime' as argument 
+						{
+							itemsForSale.splice(key,1); // this will remove item from itemsForSale.				
+							returnToUsersInventory(currentItem)
+						}
 						
-						//returnToUsersInventory(currentItem)
+					}// for		
+					
+			}, 5000);
+			
+		}//if	
+		setTimeout(function()
+		{
 
-
-						console.log(currentItem.item_name , "EXPIRED!");
-					}
-					else //THIS ELSE IS TEST CODE REMOVE	
-					{
-						console.log(currentItem.item_name , "not expired");
-					}	
-
-
-				}// for		
-			}// if		
-		}, 5000);
+			if(intervalFlag)
+				clearInterval(intervalID);
+			
+			monitorAI();
 		
-		
+		}, 250000);
+
 	});// DB		
 };// module
+
 
 //--------------------------------------
 // checks if item auction's time is up. If so returns 'true', else returns 'false'
 function isExpired(item)
-{
-	
-
+{	
 	var itemStartTime = new Date(item.createdAt);
 	var currentTime = new Date();
 	
@@ -128,41 +117,3 @@ function removeForSale(forSaleId)
 }
 
 
-
-
-/*			Interval timer notes
-
-
-			timer (little less than 5 min, function()
-			{
-				
-				stop timerid; //DOES THIS MAKE SENSE
-				
-				get itemsforsale.then(function{
-	
-					var timerid = timer(1000,function(){
-					everything else here.
-
-
-
-
-
-					})
-
-
-				
-
-				})
-
-
-
-
-
-
-			})
-
-
-
-
-
-*/
