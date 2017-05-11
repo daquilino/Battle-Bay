919c7b2051c9bd104e2dad8d6e91572d40e956c1
Daniel Cross <dancross09@gmail.com>
2017-05-10 23:33:19 -0400

    Remove debug console logs

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -10,15 +10,9 @@ const PATH = require("path");
 function LoggedIn(req)
 {
   if (req.cookies.id !== undefined)
-  {
-    console.log("There's a cookie");
     return true;
-  }
   else
-  {
-    console.log("There's no cookie");
     return false;
-  }
 }
 // Routes
 // =============================================================
@@ -26,14 +20,6 @@ module.exports = function(app)
 {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  //ADD ALL HTML ROUTES HERE 
-  /*
-    -  landing-page
-    -  sign-in-and-up page
-    -  up-for-bid Page
-    -  user-purchase page
-  */
 
   // EXAMPLE for landing page (test.html using handlebars)
   app.get("/", function(req, res) {
