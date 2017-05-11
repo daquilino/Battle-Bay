8ac5c4653d3fc314d0237a6168d45c822fdc6b91
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-05-10 21:30:22 -0400

    just refactored

--- db/warehouse-seeds.sql
+++ db/warehouse-seeds.sql
@@ -1,8 +1,6 @@
 Use battleBay_db;
 
 INSERT INTO warehouseItems (item_name, category, units_sold, price, warehouse_name) 
-VALUES ("electronics", "electronics", 25, 200, "warehouse1");
- 
-VALUES ("fashion", "fashion", 53, 15, "warehouse2");
-
-VALUES ("collectibles", "collectibles", 10, 125, "warehouse3");
+VALUES ("electronics", "electronics", 25, 200, "warehouse1"),
+("fashion", "fashion", 53, 15, "warehouse2"),
+("collectables", "collectables", 10, 125, "warehouse3");
